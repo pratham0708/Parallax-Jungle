cdb5c636957bb8e0814e1c2559ff4aa0aa848694
Pratham Saini <84400094+pratham0708@users.noreply.github.com>
2022-10-06 18:50:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var loader = document.getElementById("preloader");
+window.addEventListener("load", () => {
+  setInterval(() => {
+  
+      loader.style.display = "none";
+    
+  }, 500); })
+
+let text = document.getElementById('text');
+let bird1 = document.getElementById('bird1');
+let bird2 = document.getElementById('bird2');
+let btn = document.getElementById('btn');
+let rocks = document.getElementById('rocks');
+let forest = document.getElementById('forest');
+let water = document.getElementById('water');
+let header = document.getElementById('header');
+let sun = document.getElementById('sun');
+  
+window.addEventListener('scroll', function(){
+
+let value = window.scrollY;
+    
+text.style.top = 50 + value * -0.1 + '%';
+bird1.style.top = 10 + value * -0.2 + 'px';
+bird1.style.left = value * 1.3 + 'px';
+bird2.style.top = 50 + value * -0.5 + 'px';
+bird2.style.left = value * -5 + 'px';
+btn.style.marginTop = value * 1.5 + 'px';
+rocks.style.top = value * -0.13 + 'px';
+forest.style.top = value * 0.20 + 'px';
+
+sun.style.left = value * 2.5 + 'px';
+
+header.style.top = value * 0.5 + 'px';
+})
